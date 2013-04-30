de009d2388b0f7c85f1f22e8f7292305533f9c8c
Leonardo Valverde <leovalverde@poli.ufrj.br>
2013-04-30 17:30:18 -0300

    primeira versao (vazia)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VocalBase::Application.config.secret_token = 'de2825c88785f31e123a79e0ab7f86a477da3365d459d0f286dc02c510d55ec4bed6bf11d5291d9448d2b8ee297412cda025a71d9cd3733232ec940d7ef5d1e5'
